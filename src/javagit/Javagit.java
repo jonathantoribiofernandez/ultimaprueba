397b1952baed9140f5363d59a50ee3657f8a15d1
jtoribiofernandez <jonathan__toribio@hotmail.com>
2019-04-13 15:43:24 -0500

    ultima

---
+++ src/javagit/Javagit.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javagit;
+
+/**
+ *
+ * @author B35985
+ */
+public class Javagit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("prueba");
+    }
+    
+}
